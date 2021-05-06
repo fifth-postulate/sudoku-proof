8211b121466455788ae7a9dba273c7fdc68cec38
Daan van Berkel <daan.van.berkel@alliander.com>
2021-05-06 20:37:13 +0200

    implement a stream

---
+++ src/Stream/Kernel.elm
@@ -0,0 +1,50 @@
+module Stream.Kernel exposing (Stream, afterwards, empty, fromList, head, singleton)
+
+import Stream.Queue as Queue exposing (Queue)
+
+
+type Stream a
+    = Stream { values : List a, promises : Queue (() -> Stream a) }
+
+
+empty : Stream a
+empty =
+    Stream { values = [], promises = Queue.empty }
+
+
+singleton : a -> Stream a
+singleton value =
+    Stream { values = [ value ], promises = Queue.empty }
+
+
+fromList : List a -> Stream a
+fromList values =
+    Stream { values = values, promises = Queue.empty }
+
+
+head : Stream a -> Maybe ( a, Stream a )
+head (Stream stream) =
+    case stream.values of
+        v :: vs ->
+            Just ( v, Stream { stream | values = vs } )
+
+        [] ->
+            let
+                adjoin ( promise, promises ) =
+                    promise ()
+                        |> append (Stream { stream | promises = promises })
+            in
+            stream.promises
+                |> Queue.popFront
+                |> Maybe.map adjoin
+                |> Maybe.andThen head
+
+
+afterwards : (() -> Stream a) -> Stream a -> Stream a
+afterwards promise (Stream stream) =
+    Stream { stream | promises = Queue.pushBack promise stream.promises }
+
+
+append : Stream a -> Stream a -> Stream a
+append (Stream left) (Stream right) =
+    Stream { values = List.append left.values right.values, promises = Queue.append left.promises right.promises }
---
+++ tests/StreamTest.elm
@@ -0,0 +1,45 @@
+module StreamTest exposing (..)
+
+import Expect
+import Fuzz exposing (int, list)
+import Stream.Kernel as Stream exposing (Stream)
+import Test exposing (..)
+
+
+suite =
+    describe "Stream"
+        [ describe "integer stream" <|
+            [ test "head of natural numbers is 0" <|
+                \_ ->
+                    let
+                        actual =
+                            naturalNumbers
+                                |> Stream.head
+                                |> Maybe.map (\( n, _ ) -> n)
+                    in
+                    Expect.equal (Just 0)
+                        actual
+            , test "head of the head of natural numbers is 1" <|
+                \_ ->
+                    let
+                        actual =
+                            naturalNumbers
+                                |> Stream.head
+                                |> Maybe.map (\( _, stream ) -> stream)
+                                |> Maybe.andThen Stream.head
+                                |> Maybe.map (\( n, _ ) -> n)
+                    in
+                    Expect.equal (Just 1) actual
+            ]
+        ]
+
+
+naturalNumbers : Stream Int
+naturalNumbers =
+    naturalNumbersFrom 0
+
+
+naturalNumbersFrom : Int -> Stream Int
+naturalNumbersFrom n =
+    Stream.singleton n
+        |> Stream.afterwards (\_ -> naturalNumbersFrom <| n + 1)
