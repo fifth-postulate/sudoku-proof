cf5b9ed6ededf8ab5f633eb81e3ac3f79f1f8a35
Daan van Berkel <daan.van.berkel@alliander.com>
2023-03-03 20:00:08 +0100

    Create a NakedSingle strategy

---
+++ front/src/Sudoku/Solver/NakedSingle.elm
@@ -0,0 +1,37 @@
+module Sudoku.Solver.NakedSingle exposing (strategy)
+
+import Set exposing (Set)
+import Sudoku
+import Sudoku.Cell exposing (Cell)
+import Sudoku.Domain exposing (Domain)
+import Sudoku.Strategy exposing (Plan, Strategy)
+
+
+strategy : Strategy
+strategy problem =
+    let
+        isNakedSingle options =
+            1 == Set.size options
+
+        nakedSingles =
+            problem
+                |> Sudoku.options
+                |> List.filter (Tuple.second >> isNakedSingle)
+                |> List.concatMap toPlan
+    in
+    if List.isEmpty nakedSingles then
+        Just nakedSingles
+
+    else
+        Nothing
+
+
+toPlan : ( Cell, Set Domain ) -> Plan
+toPlan ( cell, options ) =
+    let
+        toAction option =
+            ( Sudoku.fill cell option, Set.size options )
+    in
+    options
+        |> Set.toList
+        |> List.map toAction
